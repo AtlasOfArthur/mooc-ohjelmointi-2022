c85575ec63969ce79fc6662c9de188bcafa8e777
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-09 14:57:18 +0200

    +1

--- osa04-07c_lisays_ja_poisto/src/lisays_ja_poisto.py
+++ osa04-07c_lisays_ja_poisto/src/lisays_ja_poisto.py
@@ -1,1 +1,39 @@
 # Kirjoita ratkaisu tähän
+lista = []
+userInput = ""
+laskija = 1
+
+while True:
+    print(f"Lista on nyt {lista}" )
+    userInput = input("(l)isää, (p)oista vai e(x)it: ")
+
+    if userInput == 'l':
+        if len(lista) >= 0:
+            lista.append(laskija)
+            laskija += 1
+    elif userInput == 'p':
+        if len(lista) > 0:
+            lista.pop()
+            laskija -= 1
+    elif userInput == 'x':
+        print("Moi!")
+        break
+
+# Malli vastaus
+    '''
+    lista = []
+while True:
+    print(f"Lista on nyt {lista}")
+    valinta = input("(l)isää, (p)oista vai e(x)it:")
+    if valinta == "l":
+        # alkion arvo on listan pituus + 1
+        alkio = len(lista) + 1
+        lista.append(alkio)
+    elif valinta == "p":
+        lista.pop(len(lista) - 1)
+    elif valinta == "x":
+        break
+ 
+print("Moi!")
+
+    '''
