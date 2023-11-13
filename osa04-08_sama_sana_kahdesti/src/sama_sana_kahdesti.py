6668ac9996808f01ad8523b74abe549c280139bb
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-13 12:48:58 +0200

    ..

--- osa04-08_sama_sana_kahdesti/src/sama_sana_kahdesti.py
+++ osa04-08_sama_sana_kahdesti/src/sama_sana_kahdesti.py
@@ -1,1 +1,13 @@
 # Kirjoita ratkaisu tähän
+sanat = []
+
+while True:
+    sana = input("sana: ")
+
+    if sana in sanat:
+        break
+    
+    sanat.append(sana)
+
+eri_sanat = list(set(sanat))
+print(f"Annoit {len(sanat)} eri sanaa")
--- osa04-08b_lista_kahdesti/src/lista_kahdesti.py
+++ osa04-08b_lista_kahdesti/src/lista_kahdesti.py
@@ -1,1 +1,19 @@
 # Kirjoita ratkaisu tähän
+lista = []
+sorted_lista =[]
+userInput = ""
+
+
+while True:
+
+    userInput = input("Anna luku: ")
+    lista.append(int(userInput))
+
+    if userInput == '0':
+        print("Moi!")
+        break
+
+    print(f"Lista: {lista}" )
+    sorted_lista = sorted(lista)
+    print(f"Järjestettynä: {sorted_lista}")
+
