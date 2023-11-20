0a97472904c8ffaf5e2610706aed154dc37b983c
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-20 14:02:10 +0200

    ... ...

--- osa04-20_lukulistasta_merkkijonolistaksi/src/lukulistasta_merkkijonolistaksi.py
+++ osa04-20_lukulistasta_merkkijonolistaksi/src/lukulistasta_merkkijonolistaksi.py
@@ -1,1 +1,22 @@
 # tee ratkaisu tänne
+def muotoile(liukuluvut):
+    muotoillut = [f'{luku:.2f}' for luku in liukuluvut]
+    return muotoillut
+
+# Malli vastaus
+'''
+def muotoile(lista: list):
+    tulos = []
+ 
+    for luku in lista:
+        tulos.append(f"{luku:.2f}")
+ 
+    return tulos
+
+'''
+
+
+if __name__ == "__main__":
+    lista = [1.234, 0.3333, 0.11111, 3.446]
+    muotoiltuLista = muotoile(lista)
+    print(muotoiltuLista)
