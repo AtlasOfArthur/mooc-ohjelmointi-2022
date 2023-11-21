487ec9a5494cb865243a0873c89c23c1ad91f936
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-21 13:54:58 +0200

    ..' Johan tajusin! '..

--- osa04-25_naapureita_listassa/src/naapureita_listassa.py
+++ osa04-25_naapureita_listassa/src/naapureita_listassa.py
@@ -4,7 +4,7 @@ def pisin_naapurijono(lista):
     pituus_tilapainen = 1
 
     for indx in range(1, len(lista)):
-        if lista[indx] == lista[indx - 1] + 1:
+        if lista[indx] == lista[indx - 1] + 1 or lista[indx] == lista[indx - 1] - 1:
             pituus_tilapainen += 1
         else:
             pituus_tilapainen = 1
@@ -15,6 +15,6 @@ def pisin_naapurijono(lista):
     return pituus
 
 if __name__ == "__main__":
-    lista = [1, 2, 3, 5, 6, 7, 6, 5, 6, 7, 10, 11, 10] # antaa virheen: Tulos 3 ei vastaa mallivastausta 7 testisyötteellä (1, 2, 3, 5, 6, 7, 6, 5, 6, 7, 10, 11, 10)
-    print(pisin_naapurijono(lista))                    # Silmämääräisesti katsomalla listaa, vastaus 3 on oikein, tai sitten en tajua jotain oleellista...
-                                                       # Epäilen jo virhettä tarkistuksessa!
+    lista = [1, 5, 4, 3, 4, 2, 5, 7, 6, 5, 6, 3, 4, 5, 6, 1, 0, 1, 2, 1, 2, 3, 4, 5, 6, 7, 6, 5, 6, 7, 10, 11, 10] 
+    print(pisin_naapurijono(lista))                     # ^-------------------15--------------------^
+                                                                       # Johan tajusin
