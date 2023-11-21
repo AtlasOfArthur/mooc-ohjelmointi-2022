e73714dd66e35a41fbaf3382290acc14caa7a0d8
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-21 11:49:03 +0200

    .'.'.

--- osa04-22_eniten_kirjaimia/src/eniten_kirjaimia.py
+++ osa04-22_eniten_kirjaimia/src/eniten_kirjaimia.py
@@ -1,1 +1,30 @@
 # tee ratkaisu tänne
+def eniten_kirjainta(merkkijono):
+    eniten_esiintyva_kirjain = merkkijono[0]
+    eniten_esiintymiset = merkkijono.count(eniten_esiintyva_kirjain)
+
+    for kirjain in merkkijono:
+        if merkkijono.count(kirjain) > eniten_esiintymiset:
+            eniten_esiintyva_kirjain = kirjain
+            eniten_esiintymiset = merkkijono.count(kirjain)
+
+    return eniten_esiintyva_kirjain
+
+
+# Malli vastaus
+'''
+def eniten_kirjainta(mjono: str):
+    yleisin = mjono[0]
+    for kirjain in mjono:
+        if mjono.count(kirjain) > mjono.count(yleisin):
+            yleisin = kirjain
+ 
+    return yleisin
+
+'''
+
+
+
+if __name__ == "__main__":
+    jono = "esimerkkkkimerkkkkijonokkkki"
+    print(eniten_kirjainta(jono))
