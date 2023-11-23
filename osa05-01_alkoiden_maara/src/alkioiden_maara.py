c074172203300ade81ecfd14407306606df500e6
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-23 16:54:44 +0200

    ..'-'..

--- osa05-01_alkoiden_maara/src/alkioiden_maara.py
+++ osa05-01_alkoiden_maara/src/alkioiden_maara.py
@@ -1,1 +1,19 @@
 # tee ratkaisu tänne
+def laske_alkiot(matriisi: list, alkio: int):
+    maara = 0
+    for rivi in matriisi:
+        for elementti in rivi:
+            if elementti == alkio:
+                maara += 1
+    return maara
+
+
+
+
+if __name__ == "__main__":
+      # ↓------------Matriisi-----------↓
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    #    ↑--Rivi-↑   ↑-Alkio (elementti)
+    print(laske_alkiot(m, 1))
+    vastaus = laske_alkiot(m, 1)
+    print(vastaus)
--- osa05-02_go/src/go.py
+++ osa05-02_go/src/go.py
@@ -1,1 +1,29 @@
 # tee ratkaisu tänne
+def kumpi_voitti(pelilauta):
+    pelaaja1_pisteet = 0
+    pelaaja2_pisteet = 0
+
+    for rivi in pelilauta:
+        for alkio in rivi:
+            if alkio == 1:
+                pelaaja1_pisteet += 1
+            elif alkio == 2:
+                pelaaja2_pisteet += 1
+
+    if pelaaja1_pisteet > pelaaja2_pisteet:
+        return 1
+    elif pelaaja1_pisteet < pelaaja2_pisteet:
+        return 2
+    else:
+        return 0
+    
+
+if __name__ == "__main__":
+    pelilauta = [
+    [1, 2, 1, 2],
+    [2, 1, 2, 1],
+    [1, 2, 1, 0],
+    [0, 2, 0, 1]
+    ]
+    voittaja = kumpi_voitti(pelilauta)
+    print(f"Voittaja: Pelaaja {voittaja}")
