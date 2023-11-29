906b55cc6d3ffac7354ed0608988470c42df6ec2
AtlasOfArthur <arthur.harjama@gmail.com>
2023-11-29 14:26:23 +0200

    ...^_^...

--- osa05-12_histogrammi/src/histogrammi.py
+++ osa05-12_histogrammi/src/histogrammi.py
@@ -1,1 +1,32 @@
 # tee ratkaisu tänne
+def histogrammi(m_jono: str):
+    kirjain_laskuri = {}
+    
+    # Laske kaikkien kirjainten esiintymät
+    for kirjain in m_jono:
+        if kirjain in kirjain_laskuri:
+            kirjain_laskuri[kirjain] += 1
+        else:
+            kirjain_laskuri[kirjain] = 1
+    
+    # Tulostaa histogrammin
+    for kirjain, lkm in kirjain_laskuri.items():
+        print(f"{kirjain} {'*' * lkm}")
+
+# Malli vastaus
+'''
+def histogrammi(merkkijono: str):
+    merkit = {}
+    for merkki in merkkijono:
+        if not merkki in merkit:
+            merkit[merkki] = 0
+        merkit[merkki] += 1
+ 
+    for merkki, lkm in merkit.items():
+        tahdet = "*"*lkm
+        print(f"{merkki} {tahdet}")
+
+'''
+
+if __name__ == "__main__":
+    histogrammi("digitaalibanaanikauppias")
--- osa05-15_sanakirjan_kaanto/src/sanakirjan_kaanto.py
+++ osa05-15_sanakirjan_kaanto/src/sanakirjan_kaanto.py
@@ -1,1 +1,26 @@
 # tee ratkaisu tänne
+def kaanna(sanakirja: dict):
+    kaannetty_sanakirja = {}
+    
+    for avain, arvo in sanakirja.items():
+        kaannetty_sanakirja[arvo] = avain
+    
+    sanakirja.clear()
+    sanakirja.update(kaannetty_sanakirja)
+
+# Malli ratkaisu
+'''
+def kaanna(sanakirja: dict):
+	kopio = {}
+	for avain in sanakirja:
+		kopio[avain] = sanakirja[avain]
+	for avain in kopio:
+		del sanakirja[avain]
+	for avain in kopio:
+		sanakirja[kopio[avain]] = avain
+
+'''
+if __name__ == "__main__":
+    s = {1: "eka", 2: "toka", 3: "kolmas", 4: "neljas"}
+    kaanna(s)
+    print(s)
